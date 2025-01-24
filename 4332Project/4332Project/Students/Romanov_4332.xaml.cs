e42aacf43b84ccc7d2eb47a0881026e897c76dbf
MaxRomanov007 <maksipapa999@gmail.com>
2025-01-24 16:08:09 +0300

    added Romanov window

---
+++ 4332Project/4332Project/Students/Romanov_4332.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace _4332Project
+{
+    public partial class Romanov_4332 : Window
+    {
+        public Romanov_4332()
+        {
+            InitializeComponent();
+        }
+    }
+}
