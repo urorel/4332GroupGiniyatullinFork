c5f19be51e5cbd2e319469219a58af6e92b54563
ezy4me <82320292+ezy4me@users.noreply.github.com>
2025-02-02 20:59:14 +0300

    fix conflicts

---
+++ 4332Project/4332Project/Students/Hananov_4332.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4332Project.Students
+{
+    /// <summary>
+    /// Логика взаимодействия для Hananov_4332.xaml
+    /// </summary>
+    public partial class Hananov_4332 : Window
+    {
+        public Hananov_4332()
+        {
+            InitializeComponent();
+            MessageBox.Show("Хананов");
+        }
+    }
+}
